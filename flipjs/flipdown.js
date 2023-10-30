6506e005abc80829bc5c47b8aea1b5869d9f3696
Rafipramudya <rafipramudya164@gmail.com>
2023-10-30 23:31:01 +0700

    first

--- flipjs/flipdown.js
+++ flipjs/flipdown.js
@@ -91,7 +91,7 @@ var FlipDown = function () {
   }, {
     key: "_parseOptions",
     value: function _parseOptions(opt) {
-      var headings = ["Days", "Hours", "Minutes", "Seconds"];
+      var headings = ["Month", "Days", "Hours", "Minutes"];
 
       if (opt.headings && opt.headings.length === 4) {
         headings = opt.headings;
