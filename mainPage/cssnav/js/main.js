a0809b822bd24728843ac937dc919da62f3b9f56
Rafipramudya <rafipramudya164@gmail.com>
2023-10-31 23:11:10 +0700

    first

--- mainPage/cssnav/js/main.js
+++ mainPage/cssnav/js/main.js
@@ -1,27 +1,30 @@
-/*===== MENU SHOW Y HIDDEN =====*/ 
-const navMenu = document.getElementById('nav-menu'),
-      toggleMenu = document.getElementById('nav-toggle'),
-      closeMenu = document.getElementById('nav-close')
+document.addEventListener('DOMContentLoaded', (event) => {
+    const navMenu = document.getElementById('nav-menu'),
+          toggleMenu = document.getElementById('nav-toggle'),
+          closeMenu = document.getElementById('nav-close');
 
-/*SHOW*/ 
-toggleMenu.addEventListener('click', ()=>{
-    navMenu.classList.toggle('show')
-})
+    /*SHOW*/ 
 
-/*HIDDEN*/
+    $("#nav-toggle").click(function () {
+        // $("#line-progress").css("width", "33%");
+        $("#nav-menu").addClass("show");
+      });
+
+    /*HIDDEN*/
 closeMenu.addEventListener('click', ()=>{
     navMenu.classList.remove('show')
 })
 
-/*===== ACTIVE AND REMOVE MENU =====*/
-const navLink = document.querySelectorAll('.nav__link');   
+    /*===== ACTIVE AND REMOVE MENU =====*/
+    const navLink = document.querySelectorAll('.nav__link');   
 
-function linkAction(){
-  /*Active link*/
-  navLink.forEach(n => n.classList.remove('activedd'));
-  this.classList.add('activedd');
-  
-  /*Remove menu mobile*/
-  navMenu.classList.remove('show')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction));
+    function linkAction(){
+        /*Active link*/
+        navLink.forEach(n => n.classList.remove('active'));
+        this.classList.add('active');
+
+        /*Remove menu mobile*/
+        navMenu.classList.remove('show');
+    }
+    navLink.forEach(n => n.addEventListener('click', linkAction));
+});
